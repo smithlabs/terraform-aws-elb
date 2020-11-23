9d656ab4c6b6418432b5a7ac05fc978e17f5bc9c
Sean Smith <40581796+smithlabs@users.noreply.github.com>
2020-11-23 12:55:29 -0700

    Adding elb_zone_id so module can provide var for route53 module

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "elb_security_group_id" {
   description = "The security group ID of the ELB cluster"
   value       = aws_security_group.elb.id
 }
+
+output "elb_zone_id" {
+  description = "The zone ID of the elb"
+  value       = aws_elb.elb.zone_id
+}
